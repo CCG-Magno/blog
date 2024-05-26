6494f41afe0b3753817c8b56caeab2c5c04d450f
Alejandro Rodriguez Natal <rodrigueznatalandy@gmail.com>
2024-05-25 22:24:26 -0500

    Blog Created

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import rehypeKatex from 'rehype-katex'; // relevant
+import remarkMath from 'remark-math';   // relevant
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(
+		remarkMath, // relevant
+		rehypeKatex // relevant
+	), sitemap()],
+});
