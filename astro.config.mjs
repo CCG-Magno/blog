b6ab9b3d72cf727c5d7cc593d07677a31c8068b7
Alejandro Rodriguez Natal <rodrigueznatalandy@gmail.com>
2024-05-25 22:42:52 -0500

    Blog added Deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://CCG-Magno.github.io',
+	base: 'blog',
 	integrations: [mdx(
 		remarkMath, // relevant
 		rehypeKatex // relevant
