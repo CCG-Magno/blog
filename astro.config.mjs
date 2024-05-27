504b3de2e4ce10e64a31ae2befde2666da45609c
Alejandro Rodriguez Natal <rodrigueznatalandy@gmail.com>
2024-05-27 17:24:47 -0500

    Theme - Added Dark mode & Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import rehypeKatex from 'rehype-katex'; // relevant
-import remarkMath from 'remark-math';   // relevant
+import remarkMath from 'remark-math'; // relevant
 import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://CCG-Magno.github.io',
-	base: 'blog',
-	integrations: [mdx(
-		remarkMath, // relevant
-		rehypeKatex // relevant
-	), sitemap()],
+  site: 'https://CCG-Magno.github.io',
+  base: 'blog',
+  integrations: [mdx(remarkMath,
+  // relevant
+  rehypeKatex // relevant
+  ), sitemap(), tailwind()]
 });
